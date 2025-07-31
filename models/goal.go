7737012de621f873eef6238673b5d023e0ebfdf6
Kevin Bramasta <kevinbramastaa@gmail.com>
2025-07-31 16:28:36 +0700

    refactor(models): change goal associations to pointer types

--- models/goal.go
+++ models/goal.go
@@ -9,7 +9,7 @@ type Goal struct {
     TeamID     uint      `json:"team_id" gorm:"not null"`
     GoalTime   int       `json:"goal_time" gorm:"not null"` // Goal time in minutes
     GoalType   string    `json:"goal_type" gorm:"type:varchar(20);default:'normal'"` // normal, penalty, own_goal
-    Match      Match     `json:"match,omitempty" gorm:"foreignKey:MatchID"`
-    Player     Player    `json:"player,omitempty" gorm:"foreignKey:PlayerID"`
-    Team       Team      `json:"team,omitempty" gorm:"foreignKey:TeamID"`
+    Match      *Match  `json:"match,omitempty" gorm:"foreignKey:MatchID"`
+    Player     *Player `json:"player,omitempty" gorm:"foreignKey:PlayerID"`
+    Team       *Team   `json:"team,omitempty" gorm:"foreignKey:TeamID"`
 }
