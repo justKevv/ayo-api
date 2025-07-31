57bc3838b2b6927adc039ea78bbcc21b1875af11
Kevin Bramasta <kevinbramastaa@gmail.com>
2025-07-31 15:39:10 +0700

    feat(models): extend Goal model with team_id, goal_time and goal_type fields

--- models/goal.go
+++ models/goal.go
@@ -3,10 +3,13 @@ package models
 import "gorm.io/gorm"
 
 type Goal struct {
-	gorm.Model
-	MatchID uint `gorm:"not null" json:"match_id"`
-	PlayerID uint `gorm:"not null" json:"player_id"`
-	Minute int `gorm:"not null" json:"minute"`
-	Match Match `gorm:"foreignKey:MatchID" json:"match,omitempty"`
-	Player Player `gorm:"foreignKey:PlayerID" json:"player,omitempty"`
+    gorm.Model
+    MatchID    uint      `json:"match_id" gorm:"not null"`
+    PlayerID   uint      `json:"player_id" gorm:"not null"`
+    TeamID     uint      `json:"team_id" gorm:"not null"`
+    GoalTime   int       `json:"goal_time" gorm:"not null"` // Goal time in minutes
+    GoalType   string    `json:"goal_type" gorm:"type:varchar(20);default:'normal'"` // normal, penalty, own_goal
+    Match      Match     `json:"match,omitempty" gorm:"foreignKey:MatchID"`
+    Player     Player    `json:"player,omitempty" gorm:"foreignKey:PlayerID"`
+    Team       Team      `json:"team,omitempty" gorm:"foreignKey:TeamID"`
 }
